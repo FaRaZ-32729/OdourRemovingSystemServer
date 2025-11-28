250daae4cef0bd8ef5deb804510c3692d1a8b071
FaRaZ <sial32729@gmail.com>
2025-11-28 19:48:08 +0500

    ready

---
+++ src/utils/sendEmail.js
@@ -0,0 +1,32 @@
+const nodemailer = require("nodemailer");
+const path = require("path");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const sendEmail = async (to, subject, html) => {
+    const transporter = nodemailer.createTransport({
+        host: "smtp.mailgun.org",
+        port: 587,
+        secure: false,
+        auth: {
+            user: process.env.MAILGUN_EMAIL || "support@odor.iotfiysolutions.com",
+            pass: process.env.MAILGUN_PASS,
+        },
+    });
+
+    await transporter.sendMail({
+        from: `"FrostKontroll" <${process.env.EMAIL_USER}>`,
+        to,
+        subject,
+        html,
+        attachments: [
+            {
+                filename: "logo.png",
+                path: path.join(__dirname, "../assets/logo.png"), 
+                cid: "companyLogo", 
+            },
+        ],
+    });
+};
+
+module.exports = sendEmail;
