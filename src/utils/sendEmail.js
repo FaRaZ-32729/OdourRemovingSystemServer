89d70a9635593fcf54fa64e5a5b1406452a13c2c
FaRaZ <sial32729@gmail.com>
2025-11-29 00:44:37 +0500

    ready

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,32 +1,67 @@
-const nodemailer = require("nodemailer");
+// const nodemailer = require("nodemailer");
+// const path = require("path");
+// const dotenv = require("dotenv");
+// dotenv.config();
+
+// const sendEmail = async (to, subject, html) => {
+//     const transporter = nodemailer.createTransport({
+//         host: "smtp.mailgun.org",
+//         port: 587,
+//         secure: false,
+//         auth: {
+//             user: process.env.MAILGUN_EMAIL ,
+//             pass: process.env.MAILGUN_PASS,
+//         },
+//     });
+
+//     await transporter.sendMail({
+//         from: `"FrostKontroll" <${process.env.MAILGUN_EMAIL}>`,
+//         to,
+//         subject,
+//         html,
+//         attachments: [
+//             {
+//                 filename: "logo.png",
+//                 path: path.join(__dirname, "../assets/logo.png"), 
+//                 cid: "companyLogo", 
+//             },
+//         ],
+//     });
+// };
+
+// module.exports = sendEmail;
+
+
+const formData = require("form-data");
+const Mailgun = require("mailgun.js");
+const fs = require("fs");
 const path = require("path");
-const dotenv = require("dotenv");
-dotenv.config();
+
+const mailgun = new Mailgun(formData);
+const mg = mailgun.client({
+    username: "api",
+    key: process.env.MAILGUN_API_KEY
+});
 
 const sendEmail = async (to, subject, html) => {
-    const transporter = nodemailer.createTransport({
-        host: "smtp.mailgun.org",
-        port: 587,
-        secure: false,
-        auth: {
-            user: process.env.MAILGUN_EMAIL ,
-            pass: process.env.MAILGUN_PASS,
-        },
-    });
+    try {
+        await mg.messages.create(process.env.MAILGUN_DOMAIN, {
+            from: `FrostKontroll <support@odor.iotfiysolutions.com>`,
+            to,
+            subject,
+            html,
+            attachment: [
+                {
+                    filename: "logo.png",
+                    data: fs.createReadStream(path.join(__dirname, "../assets/logo.png"))
+                }
+            ]
+        });
 
-    await transporter.sendMail({
-        from: `"FrostKontroll" <${process.env.MAILGUN_EMAIL}>`,
-        to,
-        subject,
-        html,
-        attachments: [
-            {
-                filename: "logo.png",
-                path: path.join(__dirname, "../assets/logo.png"), 
-                cid: "companyLogo", 
-            },
-        ],
-    });
+        console.log("Email sent ✔");
+    } catch (err) {
+        console.error("Mailgun error:", err);
+    }
 };
 
 module.exports = sendEmail;
