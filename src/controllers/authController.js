bc5c399a55bc56d3c84dec9b0c84873a3431540b
FaRaZ <sial32729@gmail.com>
2025-11-28 19:23:50 +0500

    ready

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -461,7 +461,7 @@ const resetPassword = async (req, res) => {
 // logout user 
 const logoutUser = async (req, res) => {
     try {
-        res.clearCookie("token", { httpOnly: true, sameSite: process.env.NODE_ENV === "production" ? "strict" : "none", secure: true });
+        res.clearCookie("token", { httpOnly: true, sameSite: "none", path: "/", secure: true });
         res.status(200).json({ success: true, message: "Logged out successfully" });
     } catch (error) {
         console.error("Error in logout:", error);
