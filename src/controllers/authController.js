3f9095edde925a627bf99da9e5191766b17bdf42
FaRaZ-32729 <sial32729@gmail.com>
2025-12-08 18:06:44 +0500

    expiresIn

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -326,7 +326,7 @@ const loginUser = async (req, res) => {
         const token = jwt.sign(
             { _id: user._id },
             process.env.JWT_SECRET,
-            { expiresIn: "7d" }
+            { expiresIn: "2m" }
         );
 
         res.cookie("token", token, {
