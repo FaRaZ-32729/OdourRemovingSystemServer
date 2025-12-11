1470ee76cf1a11e642b77511fd0148288265729a
FaRaZ-32729 <sial32729@gmail.com>
2025-12-11 18:27:08 +0500

    odour value

--- src/utils/espAlertSocket.js
+++ src/utils/espAlertSocket.js
@@ -37,7 +37,7 @@ const espAlertSocket = (server) => {
                     {
                         espTemprature: data.temperature,
                         espHumidity: data.humidity,
-                        espOdour: data.odour,
+                        espOdour: data.odor,
                         temperatureAlert: data.temperatureAlert === "HIGH",
                         humidityAlert: data.humidityAlert === "HIGH",
                         odourAlert: data.odourAlert === "DETECTED"
